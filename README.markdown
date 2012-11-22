e5a83acd71aef744ec5e221bd8f79ffc663c4ad7
Victor Pereira <vpereira@web.de>
2012-11-22 15:23:41 +0100

    xiroquerolivreninguemmecombatezerinhooo

--- README.markdown
+++ README.markdown
@@ -1,7 +1,11 @@
-Next Generation Green HyperMedia API Client - BUY NOW
+.h1 Next Generation Green HyperMedia API Client - BUY NOW
 
 some R packages are necessary. 
 
 please check the header of the file and install them. 
 
 collect info from NP for 15 minutes and plot it
+
+to run it:
+
+    R CMD BATCH parse_api.R
--- parse_api.R
+++ parse_api.R
@@ -25,20 +25,26 @@ getUpstream <- function(json) {
   mean(json$upstream)
 }
 
+getCPU <- function(json) {
+  mean(json$cpu)
+}
 
 downstream <- c()
 upstream <- c()
 timespace <- c()
+cpu <- c()
 
-for(a in 1:30) {
+for(a in 1:5) {
   json = getStates()
   timespace  <- union(timespace,getTime())
   upstream   <- union(upstream,getUpstream(json))
   downstream <- union(downstream,getDownstream(json))
+  cpu <- union(cpu,getCPU(json))
   Sys.sleep(30)
 }
 
-data = data.frame(time=timespace,downstream=downstream,upstream=upstream)
+data = data.frame(time=timespace,downstream=downstream,upstream=upstream,cpu=cpu)
+par(mfrow=c(2, 1))
 plot.new()
 plot.window(ylim=range(data$downstream),xlim=range(data$time))
 lines(data$time,data$downstream,type="l",col="red")
@@ -51,3 +57,4 @@ legend("topright",legend=c("upstream","downstream"),
 axis(1)
 axis(2)
 box()
+plot(data$time,data$cpu, main="cpu usage",type="l",ylab="cpu usage (%)",xlab="time range")
